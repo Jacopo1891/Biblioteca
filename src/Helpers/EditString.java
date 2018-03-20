890bd7b32b16335151b81a3867bef2049dfd1632
Unknown <jacopo1891@gmail.com>
2018-03-20 17:29:09 +0100

    Update CapitalizeString

--- src/Helpers/EditString.java
+++ src/Helpers/EditString.java
@@ -4,8 +4,14 @@ public final class EditString {
 
 	public static String Capitalize ( String str) {
 		/**
-		 * Capitalize first letter of a string
+		 * Capitalize first letter of each word in a string
 		 */
-		return str.substring(0, 1).toUpperCase() + str.substring(1);
+		String [] words = str.split(" ");
+		String capitalized_string = "";
+		for (int i = 0; i < words.length; i++ ) {
+			capitalized_string += words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
+			capitalized_string += (i != words.length-1 ) ? " " : "";
+		}
+		return capitalized_string;
 	}
 }
