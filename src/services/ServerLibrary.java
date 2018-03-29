cb6dfafba2dfcdc1e03bc2c43bf40867667c36c6
Unknown <jacopo1891@gmail.com>
2018-03-29 12:36:11 +0200

    Added methods to work with library

--- src/services/ServerLibrary.java
+++ src/services/ServerLibrary.java
@@ -1,5 +1,7 @@
 package services;
 
+import java.util.LinkedList;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
@@ -16,5 +18,9 @@ public interface ServerLibrary {
  
 	@WebMethod User login(String username, String password);
 	@WebMethod ComplexBooleanValue insertNewBook ( String title , String author, String publischer , User u ) ;
+	@WebMethod ComplexBooleanValue insertNewUser ( String username, String password, String role );
+	@WebMethod ComplexBooleanValue deleteBooking ( Reservation r );
+	@WebMethod LinkedList<Book> getBooksAvailable();
+	@WebMethod ComplexBooleanValue updateBook( Book b, User u );
  
 }
--- src/services/ServerLibraryImpl.java
+++ src/services/ServerLibraryImpl.java
@@ -1,5 +1,7 @@
 package services;
 
+import java.util.LinkedList;
+
 import javax.jws.WebService;
 import business_logic.*;
 import entity.*;
@@ -27,9 +29,30 @@ public class ServerLibraryImpl implements ServerLibrary {
 	
 	@Override
 	public ComplexBooleanValue insertNewBook ( String title , String author, String publischer , User u ) {
-		String[] param = {"TITLE","AUTHOR","PUBLISCHER"};
+		String[] param = {"Title","Author","Publischer"};
 		String[] value = { title, author, publischer };
 		return (ComplexBooleanValue) my_libr.insertNewBook(param, value, u);
+	}
+
+	@Override
+	public ComplexBooleanValue insertNewUser(String username, String password, String role) {
+		User new_user = new User( username, password, role );
+		return (ComplexBooleanValue) my_libr.insertNewUser( new_user );
+	}
+
+	@Override
+	public ComplexBooleanValue deleteBooking(Reservation r) {
+		return (ComplexBooleanValue)my_libr.deleteBooking( r );
+	}
+
+	@Override
+	public LinkedList<Book> getBooksAvailable() {
+		return my_libr.getBooksAvailable();
+	}
+
+	@Override
+	public ComplexBooleanValue updateBook(Book b, User u) {
+		return (ComplexBooleanValue)my_libr.updateBook( b, u);
 	}
 
 }
